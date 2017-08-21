e9fc8fb41745ca79952b989b4a1ee453ad569c41
Dylan Hennessey <dhenness@ualberta.ca>
2017-08-21 17:44:38 -0400

    added grouping options to BS_sub_auto

--- BS_sub_auto.sh
+++ BS_sub_auto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kind=''
+kind='both' # can be equal to 'm' for minus sym, 'p' for plus sym, or 'both' for both sym
 sym=''
 nuc=''
 exepath=/home/dhenness/prophet/exe
@@ -58,6 +58,10 @@ do
        wtime=$1
        shift
      ;;
+     "-k")
+       kind=$1
+       shift
+     ;;
      *)
        job=$val
      ;;
@@ -70,120 +74,48 @@ if [[ -z $nuc ]]; then
 fi
 
 if [ "$alljobs" = true ]; then
-  for file in *inp; do
-    dir=`echo $file | sed 's/_.*$//'`
-    mkdir $dir
-    cp $file $dir/$file-orig
-    OLDS=`echo $file | sed -e "s/^.*$nuc//" -e 's/s.*$//'`
-    OLDP=`echo $file | sed -e "s/^.*$nuc.*s//" -e 's/p.*$//'`
-    OLDD=`echo $file | sed -e "s/^.*$nuc.*p+//" -e 's/d.*$//'`
-    OLDF=`echo $file | sed -e "s/^.*$nuc.*d+//" -e 's/f.*$//'`
-    NEWS=$OLDS
-    NEWP=$OLDP
-    NEWD=$OLDD
-    NEWF=$OLDF
-    oldbs=`grep 'nbs' $file | sed -e 's/^.*nbs=//' -e 's/.end//'`
-    if [ "$bottom" = true ]; then
-      oldbottom=`grep 'start' $file | sed -e 's/^.*start=//' -e 's/.end//'`
-      OLDSB=`echo $oldbottom | awk '{print $1}'`
-      OLDPB=`echo $oldbottom | awk '{print $2}'`
-      OLDDB=`echo $oldbottom | awk '{print $4}'`
-      OLDFB=`echo $oldbottom | awk '{print $6}'`
-      NEWSB=$OLDSB
-      NEWPB=$OLDPB
-      NEWDB=$OLDDB
-      NEWFB=$OLDFB
-    fi
-    echo Changing directory to $dir
-    cd $dir
-    rm *.inp
-    rm *.sh
-    i=$low
-    while [ $i -le $high ]; do
-      if [ $sym == 'S' ]; then
-        let NEWS=$OLDS-$i
-      elif [ $sym == 'P' ] && [ $OLDP -ne 0 ]; then
-        let NEWP=$OLDP-$i
-      elif [ $sym == 'D' ] && [ $OLDD -ne 0 ]; then
-        let NEWD=$OLDD-$i
-      elif [ $sym == 'F' ] && [ $OLDF -ne 0 ]; then
-        let NEWF=$OLDF-$i
-      fi
-      if [ $sym == 'S' ] && [ "$bottom" = true ]; then
-        let NEWSB=$OLDSB+$i
-      elif [ $sym == 'P' ] && [ "$bottom" = true ] && [ $OLDP -ne 0 ]; then
-        let NEWPB=$OLDPB+$i
-      elif [ $sym == 'D' ] && [ "$bottom" = true ] && [ $OLDD -ne 0 ]; then
-        let NEWDB=$OLDDB+$i
-      elif [ $sym == 'F' ] && [ "$bottom" = true ] && [ $OLDF -ne 0 ]; then
-        let NEWFB=$OLDFB+$i
-      fi
-      newfile=$dir'_'$nuc$NEWS's'$NEWP'p-'$NEWP'p+'$NEWD'd-'$NEWD'd+'$NEWF'f-'$NEWF'f+.inp'
-  
-      if [ $OLDF -ne 0 ]; then
-        newbs=$NEWS' '$NEWP' '$NEWP' '$NEWD' '$NEWD' '$NEWF' '$NEWF' '
-      elif [ $OLDD -ne 0 ]; then
-        newbs=$NEWS' '$NEWP' '$NEWP' '$NEWD' '$NEWD' '
-      elif [ $OLDP -ne 0 ]; then
-        newbs=$NEWS' '$NEWP' '$NEWP' '
-      else
-        newbs=$NEWS' '
-      fi
+  job=`ls *inp`
+elif [ -z "$job" ]; then
+  echo no job given
+  exit
+fi
 
-      if [ "$bottom" = true ];then
-        if [ $OLDF -ne 0 ]; then
-          newbottom=$NEWSB' '$NEWPB' '$NEWPB' '$NEWDB' '$NEWDB' '$NEWFB' '$NEWFB' '
-        elif [ $OLDD -ne 0 ]; then
-          newbottom=$NEWSB' '$NEWPB' '$NEWPB' '$NEWDB' '$NEWDB' '
-        elif [ $OLDP -ne 0 ]; then
-          newbottom=$NEWSB' '$NEWPB' '$NEWPB' '
-        else
-          newbottom=$NEWSB' '
-        fi
-      fi
-  
-      cp $file-orig $newfile
-      sed -i -e "s/$oldbs/$newbs/" $newfile
-      if [ "$bottom" = true ];then
-        sed -i -e "s/$oldbottom/$newbottom/" $newfile
-      fi
-      make_gpu_job.sh -tt ${newfile%????} -pr $exepath/$ver \'$newfile\'
-      let i=$i+1
-    done
-    ls *_sub.sh > jobsub.sh
-    sed -i 's/^/sbatch /' jobsub.sh
-    chmod +x jobsub.sh
-    ./jobsub.sh
-    cd ..
-  done
-else
-  if [ -z "$job" ]; then
-    echo no job given
-    exit
-  fi
-  file="$job"
+for file in $job; do
   dir=`echo $file | sed 's/_.*$//'`
   mkdir $dir
   cp $file $dir/$file-orig
   OLDS=`echo $file | sed -e "s/^.*$nuc//" -e 's/s.*$//'`
-  OLDP=`echo $file | sed -e "s/^.*$nuc.*s//" -e 's/p.*$//'`
-  OLDD=`echo $file | sed -e "s/^.*$nuc.*p+//" -e 's/d.*$//'`
-  OLDF=`echo $file | sed -e "s/^.*$nuc.*d+//" -e 's/f.*$//'`
+  OLDPm=`echo $file | sed -e "s/^.*$nuc.*s//" -e 's/p.*$//'`
+  OLDPp=`echo $file | sed -e "s/^.*$nuc.*p-//" -e 's/p.*$//'`
+  OLDDm=`echo $file | sed -e "s/^.*$nuc.*p+//" -e 's/d.*$//'`
+  OLDDp=`echo $file | sed -e "s/^.*$nuc.*d-//" -e 's/d.*$//'`
+  OLDFm=`echo $file | sed -e "s/^.*$nuc.*d+//" -e 's/f.*$//'`
+  OLDFp=`echo $file | sed -e "s/^.*$nuc.*f-//" -e 's/f.*$//'`
   NEWS=$OLDS
-  NEWP=$OLDP
-  NEWD=$OLDD
-  NEWF=$OLDF
+  NEWPm=$OLDPm
+  NEWPp=$OLDPp
+  NEWDm=$OLDDm
+  NEWDp=$OLDDp
+  NEWFm=$OLDFm
+  NEWFp=$OLDFp
+
   oldbs=`grep 'nbs' $file | sed -e 's/^.*nbs=//' -e 's/.end//'`
   if [ "$bottom" = true ]; then
     oldbottom=`grep 'start' $file | sed -e 's/^.*start=//' -e 's/.end//'`
     OLDSB=`echo $oldbottom | awk '{print $1}'`
-    OLDPB=`echo $oldbottom | awk '{print $2}'`
-    OLDDB=`echo $oldbottom | awk '{print $4}'`
-    OLDFB=`echo $oldbottom | awk '{print $6}'`
+    OLDPBm=`echo $oldbottom | awk '{print $2}'`
+    OLDPBp=`echo $oldbottom | awk '{print $3}'`
+    OLDDBm=`echo $oldbottom | awk '{print $4}'`
+    OLDDBp=`echo $oldbottom | awk '{print $5}'`
+    OLDFBm=`echo $oldbottom | awk '{print $6}'`
+    OLDFBp=`echo $oldbottom | awk '{print $7}'`
     NEWSB=$OLDSB
-    NEWPB=$OLDPB
-    NEWDB=$OLDDB
-    NEWFB=$OLDFB
+    NEWPBm=$OLDPBm
+    NEWPBp=$OLDPBp
+    NEWDBm=$OLDDBm
+    NEWDBp=$OLDDBp
+    NEWFBm=$OLDFBm
+    NEWFBp=$OLDFBp
   fi
   echo Changing directory to $dir
   cd $dir
@@ -193,40 +125,71 @@ else
   while [ $i -le $high ]; do
     if [ $sym == 'S' ]; then
       let NEWS=$OLDS-$i
-    elif [ $sym == 'P' ] && [ $OLDP -ne 0 ]; then
-      let NEWP=$OLDP-$i
-    elif [ $sym == 'D' ] && [ $OLDD -ne 0 ]; then
-      let NEWD=$OLDD-$i
-    elif [ $sym == 'F' ] && [ $OLDF -ne 0 ]; then
-      let NEWF=$OLDF-$i
+    elif [ $sym == 'P' ] && [ $OLDPp -ne 0 ]; then
+      if [ "$kind" = 'm' ] || [ "$kind" = 'both' ]; then
+        let NEWPm=$OLDPm-$i
+      fi
+      if [ "$kind" = 'p' ] || [ "$kind" = 'both' ]; then
+        let NEWPp=$OLDPp-$i
+      fi
+    elif [ $sym == 'D' ] && [ $OLDDp -ne 0 ]; then
+      if [ "$kind" = 'm' ] || [ "$kind" = 'both' ]; then
+        let NEWDm=$OLDDm-$i
+      fi
+      if [ "$kind" = 'p' ] || [ "$kind" = 'both' ]; then
+        let NEWDp=$OLDDp-$i
+      fi
+    elif [ $sym == 'F' ] && [ $OLDFp -ne 0 ]; then
+      if [ "$kind" = 'm' ] || [ "$kind" = 'both' ]; then
+        let NEWFm=$OLDFm-$i
+      fi
+      if [ "$kind" = 'p' ] || [ "$kind" = 'both' ]; then
+        let NEWFp=$OLDFp-$i
+      fi
     fi
     if [ $sym == 'S' ] && [ "$bottom" = true ]; then
       let NEWSB=$OLDSB+$i
-    elif [ $sym == 'P' ] && [ "$bottom" = true ] && [ $OLDP -ne 0 ]; then
-      let NEWPB=$OLDPB+$i
-    elif [ $sym == 'D' ] && [ "$bottom" = true ] && [ $OLDD -ne 0 ]; then
-      let NEWDB=$OLDDB+$i
-    elif [ $sym == 'F' ] && [ "$bottom" = true ] && [ $OLDF -ne 0 ]; then
-      let NEWFB=$OLDFB+$i
+    elif [ $sym == 'P' ] && [ "$bottom" = true ] && [ $OLDPp -ne 0 ]; then
+      if [ "$kind" = 'm' ] || [ "$kind" = 'both' ]; then
+        let NEWPBm=$OLDPBm+$i
+      fi
+      if [ "$kind" = 'p' ] || [ "$kind" = 'both' ]; then
+        let NEWPBp=$OLDPBp+$i
+      fi
+    elif [ $sym == 'D' ] && [ "$bottom" = true ] && [ $OLDDp -ne 0 ]; then
+      if [ "$kind" = 'm' ] || [ "$kind" = 'both' ]; then
+        let NEWDBm=$OLDDBm+$i
+      fi
+      if [ "$kind" = 'p' ] || [ "$kind" = 'both' ]; then
+        let NEWDBp=$OLDDBp+$i
+      fi
+    elif [ $sym == 'F' ] && [ "$bottom" = true ] && [ $OLDFp -ne 0 ]; then
+      if [ "$kind" = 'm' ] || [ "$kind" = 'both' ]; then
+        let NEWFBm=$OLDFBm+$i
+      fi
+      if [ "$kind" = 'p' ] || [ "$kind" = 'both' ]; then
+        let NEWFBp=$OLDFBp+$i
+      fi
     fi
-    newfile=$dir'_'$nuc$NEWS's'$NEWP'p-'$NEWP'p+'$NEWD'd-'$NEWD'd+'$NEWF'f-'$NEWF'f+.inp'
+    newfile=$dir'_'$nuc$NEWS's'$NEWPm'p-'$NEWPp'p+'$NEWDm'd-'$NEWDp'd+'$NEWFm'f-'$NEWFp'f+.inp'
 
-    if [ $OLDF -ne 0 ]; then
-      newbs=$NEWS' '$NEWP' '$NEWP' '$NEWD' '$NEWD' '$NEWF' '$NEWF' '
-    elif [ $OLDD -ne 0 ]; then
-      newbs=$NEWS' '$NEWP' '$NEWP' '$NEWD' '$NEWD' '
-    elif [ $OLDP -ne 0 ]; then
-      newbs=$NEWS' '$NEWP' '$NEWP' '
+    if [ $OLDFp -ne 0 ]; then
+      newbs=$NEWS' '$NEWPm' '$NEWPp' '$NEWDm' '$NEWDp' '$NEWFm' '$NEWFp' '
+    elif [ $OLDDp -ne 0 ]; then
+      newbs=$NEWS' '$NEWPm' '$NEWPp' '$NEWDm' '$NEWDp' '
+    elif [ $OLDPp -ne 0 ]; then
+      newbs=$NEWS' '$NEWPm' '$NEWPp' '
     else
       newbs=$NEWS' '
     fi
+
     if [ "$bottom" = true ];then
-      if [ $OLDF -ne 0 ]; then
-        newbottom=$NEWSB' '$NEWPB' '$NEWPB' '$NEWDB' '$NEWDB' '$NEWFB' '$NEWFB' '
-      elif [ $OLDD -ne 0 ]; then
-        newbottom=$NEWSB' '$NEWPB' '$NEWPB' '$NEWDB' '$NEWDB' '
-      elif [ $OLDP -ne 0 ]; then
-        newbottom=$NEWSB' '$NEWPB' '$NEWPB' '
+      if [ $OLDFp -ne 0 ]; then
+        newbottom=$NEWSB' '$NEWPBm' '$NEWPBp' '$NEWDBm' '$NEWDBp' '$NEWFBm' '$NEWFBp' '
+      elif [ $OLDDp -ne 0 ]; then
+        newbottom=$NEWSB' '$NEWPBm' '$NEWPBp' '$NEWDBm' '$NEWDBp' '
+      elif [ $OLDPp -ne 0 ]; then
+        newbottom=$NEWSB' '$NEWPBm' '$NEWPBp' '
       else
         newbottom=$NEWSB' '
       fi
@@ -234,7 +197,7 @@ else
 
     cp $file-orig $newfile
     sed -i -e "s/$oldbs/$newbs/" $newfile
-    if [ "$bottom" = true ]; then
+    if [ "$bottom" = true ];then
       sed -i -e "s/$oldbottom/$newbottom/" $newfile
     fi
     make_gpu_job.sh -tt ${newfile%????} -pr $exepath/$ver \'$newfile\' -t $wtime
@@ -243,7 +206,6 @@ else
   ls *_sub.sh > jobsub.sh
   sed -i 's/^/sbatch /' jobsub.sh
   chmod +x jobsub.sh
-  ./jobsub.sh
+#  ./jobsub.sh
   cd ..
-fi
-
+done
